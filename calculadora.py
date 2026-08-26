a36bb29e3d5472296be7fe76a7db09318ed8a4ef
Fael904 <rvilela1520@gmail.com>
2026-08-25 20:44:36 -0400

    Calculadora pronta

---
+++ calculadora.py
@@ -0,0 +1,52 @@
+from soma import Soma
+from subtracao import Subtracao
+from divisao import Divisao
+from multiplicacao import Multiplicacao
+from potenciacao import Potenciacao
+ 
+OPCOES = {
+    "1": Soma,
+    "2": Subtracao,
+    "3": Divisao,
+    "4": Multiplicacao,
+    "5": Potenciacao
+ 
+}
+def ler_numero(mensagem):
+    while True:
+        try:
+            return float(input(mensagem).replace(",","."))
+        except ValueError:
+            print("Valor Invalido, digite um numero")
+ 
+def ler_opcao():
+    print("\n=== Calculadora ===")
+    for chave, classe in OPCOES.items():
+        print(f"{chave} - {classe.nome}")
+    print("0 - Sair")
+ 
+    while True:
+        escolha = input("Escolha uma opções: ").strip()
+        if escolha == "0" or escolha in OPCOES:
+            return escolha
+        print("Opção Invalida")
+ 
+def main():
+    while True:
+        escolha = ler_opcao()
+        if escolha == "0":
+            print("Até mais")
+            break
+ 
+        a = ler_numero("Digite um Numero: ")
+        b = ler_numero("Digite outro Numero: ")
+ 
+        operacao = OPCOES[escolha](a,b)
+ 
+        try:
+            print(f"Resultado:{operacao}")
+        except ValueError as erro:
+            print(f"Erro:{erro}")
+ 
+if __name__ == "__main__":
+    main()
